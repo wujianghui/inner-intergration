5beb75a72f776eafa86dd0a9fe281ee3a35eaf45
wujianghui <583379737@qq.com>
2020-05-30 18:47:25 +0800

    chushihua

---
+++ uaa-client-spring-boot-starter/src/main/java/com/open/capacity/uaa/client/constant/AuthServiceConstant.java
@@ -0,0 +1,16 @@
+package com.open.capacity.uaa.client.constant;
+
+/**
+ *
+ * @author 作者 owen 
+ * @version 创建时间：2017年12月4日 下午5:32:29
+ * 鉴权服务 
+ * 与spring.application.name一致
+ * blog: https://blog.51cto.com/13005375 
+ * code: https://gitee.com/owenwangwen/open-capacity-platform
+ */
+
+public class AuthServiceConstant {
+	public static final String GATEWAY_SERVICE = "api-gateway";
+	public static final String AUTH_SERVICE = "auth-server";
+}
